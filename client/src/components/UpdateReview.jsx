31ceef4ba3f11f5a2fecca6c4ac88267ceb4d1f8
Koa Garces <koajgarces@yahoo.com>
2022-04-01 09:21:49 -0700

    updated update controller

--- client/src/components/UpdateReview.jsx
+++ client/src/components/UpdateReview.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 
 const UpdateReview = () => {
-  const createNewElement = () => {
-    let newInputBox = document.createElement("div");
-    newInputBox.innerHTML = <input type="text" id="newInputBox"></input>;
-    document.getElementById("newElementId").appendChild(newInputBox);
-  };
-
   return (
     <main>
       <div className="Update Button">
-        <button value="Create Element" onclick="createNewElement();">
-          Update
-        </button>
+        {/* <Input
+          type: "comment"
+          _id: 
+            /> */}
+        <button>Update</button>
       </div>
-      <div id="newElementId"></div>
     </main>
   );
 };
