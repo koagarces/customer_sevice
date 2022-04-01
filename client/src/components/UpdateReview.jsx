641bb7d1b150f3f5f3eaff3027fff3784e9d41e5
Koa Garces <koajgarces@yahoo.com>
2022-04-01 08:29:02 -0700

    created function for update controller

---
+++ client/src/components/UpdateReview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateReview = () => {
+  return (
+    <div className="deleteButton">
+      <button>Update Review</button>
+    </div>
+  );
+};
+
+export default UpdateReview;
